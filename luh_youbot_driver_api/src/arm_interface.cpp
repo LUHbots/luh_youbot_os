33859e07784c83701b8cc9974460b3d00018c020
Andrej Dick <dick.andrej22@gmail.com>
2016-04-09 12:42:20 +0200

    Versuch Bug mit nicht angezeigten Figer zu beheben

--- luh_youbot_driver_api/src/arm_interface.cpp
+++ luh_youbot_driver_api/src/arm_interface.cpp
@@ -154,6 +154,14 @@ void YoubotArmInterface::initialise(bool use_standard_gripper, bool use_luh_grip
         if(use_luh_gripper_v3_)
         {
             int slaveNumber=9; //Erstmal hardgecoded TODO
+            std::string gripper_bar_name;
+            gripper_bar_name="finger_joint_l";
+            gripper_finger_names_[LEFT_FINGER_INDEX] = gripper_bar_name;
+            ROS_INFO("Joint %i for gripper of arm %s has name: %s", 1, name_.c_str(), gripper_bar_name.c_str());
+            gripper_bar_name="finger_joint_r";
+            gripper_finger_names_[RIGHT_FINGER_INDEX] = gripper_bar_name;
+            ROS_INFO("Joint %i for gripper of arm %s has name: %s", 2, name_.c_str(), gripper_bar_name.c_str());
+
 
             luh_gripper_v3_=new arduino_gripper();
             luh_gripper_v3_->initAdruinoGripper(slaveNumber,config_->config_path); //TODO Slave Number
